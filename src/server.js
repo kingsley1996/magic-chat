6f889067755e0444decf20ff92c98dd5d56af409
Your Name Here <linhdang.dev96@gmail.com>
2019-07-28 12:37:12 +0700

    add static templates

--- src/server.js
+++ src/server.js
@@ -1,23 +1,38 @@
 import express from "express";
 import ConnectDB from "./config/connectDB";
-import ContactModel from "./models/contact.model";
+import configViewEngine from "./config/viewEngine";
 
 let app = express();
 
 // Connect to mongodb
 ConnectDB();
 
-app.get("/test-database", async (req, res) => {
-   try {
-       let item = {
-           userId: "15402344",
-           contactId: "17243254305438543"
-       };
-       let contact = await ContactModel.createNew(item);
-       res.send(contact);
-   } catch (err) {
-       console.log(err);
-   }
+//Config view engine
+configViewEngine(app);
+
+// app.get("/test-database", async (req, res) => {
+//    try {
+//        let item = {
+//            userId: "15402344",
+//            contactId: "17243254305438543"
+//        };
+//        let contact = await ContactModel.createNew(item);
+//        res.send(contact);
+//    } catch (err) {
+//        console.log(err);
+//    }
+// });
+
+app.get("/", (req, res) => {
+    return res.render("main/master");
+});
+
+app.get("/login", (req, res) => {
+    return res.render("auth/login");
+});
+
+app.get("/sign-up", (req, res) => {
+    return res.render("auth/signup");
 });
 
 app.listen(process.env.APP_PORT, process.env.APP_HOST, () => {
